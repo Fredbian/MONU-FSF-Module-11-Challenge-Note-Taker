35fc5bcf18ceaa3980490f38c4564a9433d3e8b5
Tao Bian <biantao0519@hotmail.com>
2022-07-19 18:17:54 +1000

    working on html router

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,22 @@
 // modules
 const router = require('express').Router()
+const fs = require('fs')
 const uuid = require('uuid')
 const db = require('../db/db.json')
+
+// function for reading notes
+const readNotes = () => {
+    return JSON.parse(fs.readFile(db) || '[]')
+}
+
+// function for creating new notes
+const createNewNotes = () => {
+    
+}
+
+// function for deleting notes
+
+
 
 // set router for notes
 router.get('/api/notes', (req, res) => {
